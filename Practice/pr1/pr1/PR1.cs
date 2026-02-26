ea31a817993ad039a7d4181cf229055c9f31a7ef
Ilya <push.3124567890@gmail.com>
2026-02-26 17:07:51 +0200

    Task 1 complete

---
+++ Practice/pr1/pr1/PR1.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Practice
+{
+    public class PR1
+    {
+        public delegate double MathOperation(double a, double b);
+        public void Example()
+        {
+            MathOperation mathOperation = Add;
+            Console.WriteLine(mathOperation(5, 3));
+
+            mathOperation = Subtract;
+            Console.WriteLine(mathOperation(5, 3));
+
+            mathOperation = Multiply;
+            Console.WriteLine(mathOperation(5, 3));
+
+            mathOperation = Divide;
+            Console.WriteLine(mathOperation(5, 3));
+        }
+
+        private double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        private double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        private double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        private double Divide(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Practice/pr1/pr1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main()
+        {
+            PR1 pr1 = new PR1();
+            pr1.Example();
+        }
+    }
+}
