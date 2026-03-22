c26d6356a5073674e58e2af925df737d456e675c
Ilya <push.3124567890@gmail.com>
2026-03-22 23:48:22 +0200

    Task3 Completed

--- Practice/pr3/pr3/Program.cs
+++ Practice/pr3/pr3/Program.cs
@@ -7,8 +7,11 @@
             /*Task1 task1 = new Task1();
             task1.Example();*/
 
-            Task2 task2 = new Task2();
-            task2.Inspector("E:\\Programs Project\\krita\\pot and lycoris");
+            /*Task2 task2 = new Task2();
+            task2.Inspector("E:\\Programs Project\\krita\\pot and lycoris");*/
+
+            Task3 task3 = new Task3();
+            task3.Example("E:\\Photos");
         }
     }
 }
---
+++ Practice/pr3/pr3/Task3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr3
+{
+    public class Task3
+    {
+        public FileInfo Search(string path)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(Path.GetFullPath(path));
+            FileInfo fileInfo = null;
+            foreach (DirectoryInfo directory in directoryInfo.GetDirectories())
+            {
+                FileInfo candidate = Search(directory.FullName);
+                if (candidate != null)
+                {
+                    if (fileInfo == null || candidate.Length > fileInfo.Length)
+                        fileInfo = candidate;
+                }
+            }
+
+            foreach (FileInfo file in directoryInfo.GetFiles())
+            {
+                if (fileInfo == null || file.Length > fileInfo.Length)
+                {
+                    fileInfo = file;
+                }
+            }
+
+            return fileInfo;
+        }
+
+        public void Example(string path)
+        {
+            FileInfo largestFile = Search(path);
+            Console.WriteLine($"The largest file is {largestFile.Name} {largestFile.Length} bytes, {largestFile.FullName}");
+        }
+    }
+}
