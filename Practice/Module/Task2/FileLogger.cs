377714c4eade4274fde41052e4e7fb2250e06348
Ilya <push.3124567890@gmail.com>
2026-03-26 12:34:16 +0200

    add time

--- Practice/Module/Task2/FileLogger.cs
+++ Practice/Module/Task2/FileLogger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +20,7 @@ namespace Task2
 
         public void OnMessageSent(object sender, MessageSentEventArgs e)
         {
-            File.AppendAllText(_filePath, e.Message + "\n");
+            File.AppendAllText(_filePath, "Time: " + DateTime.Now + ": " + e.Message + "\n");
         }
     }
 }
